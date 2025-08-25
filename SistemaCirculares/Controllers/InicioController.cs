2a95638ef6ba7c3d02ef71a5f3943cff81f94a96
Noemi-Murillo <143678256+Noemi-Murillo@users.noreply.github.com>
2025-08-24 18:57:38 -0600

    Agregar archivos de proyecto.

---
+++ SistemaCirculares/Controllers/InicioController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace SistemaCirculares.Controllers
+{
+    public class InicioController : Controller
+    {
+        public IActionResult Iniciosesion()
+        {
+            return View();
+        }
+    }
+}
