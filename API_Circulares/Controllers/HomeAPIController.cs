0a6df1b1decb46e16a56663a48809f9893d8e1ce
Noemí Murillo <noemi.murillo.godinez@est.una.ac.cr>
2025-11-13 11:25:38 -0600

    Feat: Se realizan ajustes a API Circulares para mostrar una página estática y no mostrar el json del controlador por defecto

---
+++ API_Circulares/Controllers/HomeAPIController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace API_Circulares.Controllers
+{
+    public class HomeAPIController : Controller
+    {
+        public IActionResult API_Circulares()
+        {
+            return View();
+        }
+    }
+}
--- API_Circulares/Program.cs
+++ API_Circulares/Program.cs
@@ -1,12 +1,10 @@
-using BLL_Circulares;
+﻿using BLL_Circulares;
 using DAL_Circulares;
 using Utils_Circulares.GeneradorAleatorio;
 
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
-
-builder.Services.AddControllers();
+builder.Services.AddControllersWithViews();
 
 builder.Services.AddScoped<InicioBLL>();
 builder.Services.AddScoped<InicioDAL>();
@@ -17,13 +15,19 @@ builder.Services.AddScoped<PublicarCircularDAL>();
 builder.Services.AddScoped<CircularesBLL>();
 builder.Services.AddScoped<CircularesDAL>();
 
-
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
+app.UseStaticFiles();
 
+app.UseHttpsRedirection();
 app.UseAuthorization();
 
+// ⬅️ Ruta convencional para Vistas (Home/Index por defecto)
+app.MapControllerRoute(
+    name: "default",
+    pattern: "{controller=HomeAPI}/{action=API_Circulares}/{id?}");
+
+// ⬅️ Tus controladores con rutas por atributo siguen funcionando
 app.MapControllers();
 
 app.Run();
